979538cd98b4d37d0d589984012b93724648e450
Whien <sal95610@gmail.com>
2018-06-27 18:24:12 +0800

    Example: add 3
    * sample_type
    * sample_len
    * sample_rang

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,42 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
+  sample_type();
+  // sample_len();
+  // sample_rang();
+}
+
+/*
+  Type System
+
+ Length 	Signed 	Unsigned
+  8-bit 	i8 	u8
+  16-bit 	i16 	u16
+  32-bit 	i32 	u32
+  64-bit 	i64 	u64
+  arch 	isize 	usize
+
+  f32
+  f64 (default is float 64bit)
+
+  bool
+
+  char
+ */
+fn sample_type() {
+  The Tuple Type
+  let tup: (i32, bool) = (500, true);
+  let first = tup.0;
+  let (x, y) = tup;
+  println!("{}-{}-{}", x, y, first);
+}
+
+fn sample_len() {
+  let spaces = "   ";
+  println!("{}", spaces.len())
+}
+
+fn sample_rang() {
   let secret_number = rand::thread_rng().gen_range(1, 101);
 
   loop {
